bc89ef9b5e4f7e9f65e9f0cd96fa8a7bf61e0092
Roman Kulichkov <roman@kulichkov.pro>
2016-08-08 13:11:25 +0700

    Шаблон списка товаров.

---
+++ bitrix/templates/bitrix-ripe/components/bitrix/catalog/main/bitrix/catalog.section/.default/template.php
@@ -0,0 +1,42 @@
+<?php
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
+	die();
+
+if (!empty($arResult['ITEMS'])) {
+	?>
+	<div id="section-product" class="row">
+		<? foreach ($arResult['ITEMS'] as $arItem) {
+			if (is_array($arItem)) {
+				$bPicture = is_array($arItem["PREVIEW_PICTURE"]);
+				?>
+				<a class="product-preview col-lg-2" itemscope itemtype="http://schema.org/Product" href="<?= $arItem["DETAIL_PAGE_URL"] ?>">
+					<div class="image">
+						<div class="fixer">
+							<? if ($bPicture): ?>
+								<img itemprop="image" src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" width="<?= $arItem["PREVIEW_PICTURE"]["WIDTH"] ?>" height="<?= $arElement["PREVIEW_PICTURE"]["HEIGHT"] ?>" alt="<?= $arElement["NAME"] ?>">
+								<div class="zoom"></div>
+							<? else: ?>
+								<div class="no-photo-div-big"></div>
+							<?endif ?>
+						</div>
+					</div>
+
+					<div class="info">
+						<div class="title" itemprop="name"><?= $arItem["NAME"] ?></div>
+						<div class="category">Оптовая цена</div>
+						<div class="cost">
+							<span class="pair"><?= needleMoneyFormat($arItem["PROPERTIES"]["PAIR_PRICE"]["VALUE"]) ?></span>
+							<?= ($oldPrice = $arItem["PROPERTIES"]["OLD_PRICE"]["VALUE"]) ? "<span class=\"old-price\">" . needleMoneyFormat($oldPrice) . "</span>" : "" ?>
+						</div>
+					</div>
+				</a>
+			<? } ?>
+		<? } ?>
+	</div>
+<?
+}
+
+if ($arParams["DISPLAY_BOTTOM_PAGER"]) {
+	echo $arResult["NAV_STRING"]; echo $arResult["DESCRIPTION"];
+}
